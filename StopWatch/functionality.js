e0c21615dd89bc36c071bff8a95872c9c017412c
AvinashC-23 <avinashc212002@gmail.com>
2023-04-11 15:10:06 +0530

    javascript added

--- StopWatch/functionality.js
+++ StopWatch/functionality.js
@@ -1,3 +1,22 @@
+
+
+const modes=document.getElementById('modeSwitch');
+const body=document.querySelector('body');
+
+modes.addEventListener('click',function(){
+    this.classList.toggle('fa-moon');
+    if(this.classList.toggle('fa-solid fa-sun')){
+        body.style.background='linear-gradient(90deg, rgba(177,185,185,1) 0%, rgba(240,238,231,1) 100%)';
+        body.style.color= rgb(90, 95, 99);
+        body.style.transition='2s';
+    }
+    else{
+        body.style.background=rgb(59,61,61);
+        body.style.background='linear-gradient(90deg, rgba(59,61,61,1) 0%, rgba(0,0,0,1) 100%';
+        body.style.color= rgb(90, 95, 99);
+        body.style.transition='2s';
+    }
+});
 function displayTime(){
     var dateTime =new Date();
     var hour=dateTime.getHours();
@@ -9,4 +28,6 @@ function displayTime(){
     document.getElementById('secs').innerHTML=sec;
 
 }
-setInterval(displayTime,10)
+setInterval(displayTime,10);
+
+
